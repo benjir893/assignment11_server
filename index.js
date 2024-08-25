7af4393db439685326823e59ff11bb9cfa2da1bc
benjir bhuyan <benjir_bhuyan@yahoo.com>
2024-08-26 04:44:38 +0800

    created mongodb for server api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,8 +8,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// assignment11
+// kvsb4AItKAMGLrTZ
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.mym2gsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
