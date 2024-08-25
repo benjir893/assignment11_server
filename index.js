053542b2e24934b8e7b233fc83d754244bc9a527
benjir bhuyan <benjir_bhuyan@yahoo.com>
2024-08-26 04:52:48 +0800

    created doteenv file and connected mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,7 +13,7 @@ app.use(express.json());
 // kvsb4AItKAMGLrTZ
 
 
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.mym2gsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mym2gsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
