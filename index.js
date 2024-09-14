0977954241871887636593404dc393b12ad39051
benjir bhuyan <benjir_bhuyan@yahoo.com>
2024-09-14 13:53:55 +0800

    added get blog api

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
     const assignment11db = client.db('assignment11db').collection('queryproduct');
     const assignment11db_users = client.db('assignment11db').collection('users');
     const assignment11db_recomendation = client.db('assignment11db').collection('recomendation');
+    const assignment11db_blog = client.db('assignment11db').collection('blogs');
 
     // get api's
     app.get('/users', async (req, res) => {
@@ -48,15 +49,20 @@ async function run() {
       const result = await assignment11db.findOne(query);
       res.send(result);
     })
-    app.get('/recomendation', async(req, res)=>{
+    app.get('/recomendation', async (req, res) => {
       const cursor = assignment11db_recomendation.find();
-      const result =await cursor.toArray();
+      const result = await cursor.toArray();
       res.send(result)
     })
-    app.get('/recomendation/:id', async(req, res)=>{
+    app.get('/recomendation/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const result =await assignment11db_recomendation.findOne(query);
+      const query = { _id: new ObjectId(id) };
+      const result = await assignment11db_recomendation.findOne(query);
+      res.send(result);
+    })
+    app.get('/blogs', async (req, res) => {
+      const cursor = assignment11db_blog.find();
+      const result = await cursor.toArray();
       res.send(result);
     })
     //delete api's
@@ -66,13 +72,13 @@ async function run() {
       const result = await assignment11db.deleteOne(query);
       res.send(result);
     })
-    app.delete('/recomendation/:id', async(req,res)=>{
+    app.delete('/recomendation/:id', async (req, res) => {
       const id = req.params.id;
-      const query ={id: new ObjectId(id)};
-      const result =await assignment11db_recomendation.deleteOne(query);
+      const query = { id: new ObjectId(id) };
+      const result = await assignment11db_recomendation.deleteOne(query);
       res.send(result)
     })
-    app.put('/queryproduct/:id', async(req, res) => {
+    app.put('/queryproduct/:id', async (req, res) => {
       const id = req.params.id;
       const updatedQuery = req.body;
       const query = { _id: new ObjectId(id) };
@@ -90,11 +96,11 @@ async function run() {
           // email: updatedQuery.email,
         }
       }
-      const result =await assignment11db.updateOne(query, prod, options);
+      const result = await assignment11db.updateOne(query, prod, options);
       res.send(result);
     })
     //post api's
-    app.post('/recomendation', async(req, res)=>{
+    app.post('/recomendation', async (req, res) => {
       const newquery = req.body;
       const result = assignment11db_recomendation.insertOne(newquery);
       res.send(result)
@@ -108,6 +114,11 @@ async function run() {
       const newUser = req.body;
       const result = await assignment11db_users.insertOne(newUser);
       res.send(result);
+    })
+    app.post('/blogs', async (req, res) => {
+      const newblog = req.body;
+      const result =await assignment11db_blog.insertOne(newblog);
+      res.send(result)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
